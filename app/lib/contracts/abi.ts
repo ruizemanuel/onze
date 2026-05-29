b21aa8554d6026ad6b368980bf47e7bac9d65dfa
ruizemanuel <ruiz.e.642@gmail.com>
2026-05-29 20:38:37 -0300

    feat(app): regenerate V2 Pick5Pool ABI + add factory ABI

--- app/lib/contracts/abi.ts
+++ app/lib/contracts/abi.ts
@@ -1,7 +1,10 @@
 import Pick5PoolJson from "./Pick5Pool.json";
+import Pick5PoolFactoryJson from "./Pick5PoolFactory.json";
 import CoachAgentJson from "./CoachAgent.json";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const pick5PoolAbi = Pick5PoolJson.abi as readonly any[];
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const pick5FactoryAbi = Pick5PoolFactoryJson.abi as readonly any[];
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const coachAgentAbi = CoachAgentJson.abi as readonly any[];
